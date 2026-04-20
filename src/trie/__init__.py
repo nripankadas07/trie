49092527d9c8ff3e79a61fde2ea5755d501c0ce7
Dr Nripanka Das <nripankadas@gmail.com>
2026-04-20 20:34:25 +0400

    Add src/trie/__init__.py

---
+++ src/trie/__init__.py
@@ -0,0 +1,8 @@
+"""Trie - A prefix tree data structure for fast prefix search and autocomplete."""
+
+from trie.core import Trie
+from trie.errors import TrieError
+
+__version__ = "1.0.0"
+__author__ = "nripankadas07"
+__all__ = ["Trie", "TrieError"]
